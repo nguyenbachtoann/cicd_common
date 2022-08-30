828fd692d4deeb407389d94d89c416a899ec4786
nguyenbachtoann <nguyenbachtoan120997@gmail.com>
2022-08-30 23:01:26 +0700

    update jenkins file

--- demo/dev/music-card-dev.groovy
+++ demo/dev/music-card-dev.groovy
@@ -1,5 +1,17 @@
 node {
     stage('Pull Code') {
-        echo 'pulling'
+        echo 'pulling...'
+    }
+    stage('Unit Test') {
+        echo 'testing...'
+    }
+    stage('Build Image & Push To Docker Hub') {
+        echo 'building & pushing'
+    }
+    stage('Deploy') {
+        echo 'deploying'
+    }
+    stage('Code Scan') {
+        echo 'scanning'
     }
 }
