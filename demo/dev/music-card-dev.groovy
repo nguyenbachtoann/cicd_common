c7e1328f0b0fbc711232f3b02180b053f39572c1
nguyenbachtoann <nguyenbachtoan120997@gmail.com>
2022-08-30 22:06:51 +0700

    update jenkins file

--- demo/dev/music-card-dev.groovy
+++ demo/dev/music-card-dev.groovy
@@ -1,32 +1,10 @@
 pipeline {
+    agent any
+
     stages {
         stage('Pull Code') {
             steps {
                 echo 'Pulling..'
-            }
-        }
-
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
-        }
-
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-
-        stage('Code Scan') {
-            steps {
-                echo 'Scanning....'
             }
         }
     }
