799c36919fba3149953914139c3264fa69888fe0
nguyenbachtoann <nguyenbachtoan120997@gmail.com>
2022-08-30 21:43:04 +0700

    update step

--- demo/dev/music-card-dev.groovy
+++ demo/dev/music-card-dev.groovy
@@ -1,1 +1,35 @@
-echo 'halo from cicd_common'
+pipeline {
+    agent any
+
+    stages {
+        stage('Pull Code') {
+            steps {
+                echo 'Pulling..'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Building..'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Deploying....'
+            }
+        }
+
+        stage('Code Scan') {
+            steps {
+                echo 'Scanning....'
+            }
+        }
+    }
+}
